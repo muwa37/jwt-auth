f3fb6cce2e65820c42270e4bcb9803e824561511
muwa37 <yond41m3@icloud.com>
2024-04-24 12:37:45 +0400

    Modified services and controller

--- server/services/user-service.js
+++ server/services/user-service.js
@@ -2,6 +2,8 @@ const UserModel = require('../models/user-model');
 const bcrypt = require('bcrypt');
 const uuid = require('uuid');
 const mailService = require('./mail-service');
+const tokenService = require('./token-service');
+const UserDto = require('../dtos/user-dto');
 
 class UserService {
   async registration(email, password) {
@@ -11,12 +13,22 @@ class UserService {
     }
     const hashPassword = await bcrypt.hash(password, 3);
     const activationLink = uuid.v4();
+
     const user = await UserModel.create({
       email,
       password: hashPassword,
       activationLink,
     });
     await mailService.sendActivationMail(email, activationLink);
+
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateToken({ ...userDto });
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return {
+      ...tokens,
+      user: userDto,
+    };
   }
 }
 
