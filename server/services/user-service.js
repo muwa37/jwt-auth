5abac4e850baac9660129909e5d3b7df9ac488ed
muwa37 <yond41m3@icloud.com>
2024-04-25 22:42:47 +0500

    Modified activation

--- server/services/user-service.js
+++ server/services/user-service.js
@@ -33,6 +33,15 @@ class UserService {
       user: userDto,
     };
   }
+
+  async activate(activationLink) {
+    const user = await UserModel.findOne(activationLink);
+    if (!user) {
+      throw new Error('wrong activation link');
+    }
+    user.isActivated = true;
+    await user.save();
+  }
 }
 
 module.exports = new UserService();
